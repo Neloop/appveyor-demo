58bcbf30415293371b37f5b24daf8956b6a87f73
Martin Polanka <PolankaMartin@gmail.com>
2017-03-10 18:15:20 +0100

    project init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appveyor_demo {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
